8412affd571303be5fe4f16e9e5af1846fe567ea
guidsantos <guilherme.fast2503@gmail.com>
2021-03-15 20:45:56 -0300

    Create Deposit and Withdraw function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const express = require('express')
 const { v4: uuidv4 } = require('uuid')
 
@@ -5,7 +6,21 @@ const app = express()
 
 app.use(express.json())
 
-const customers = [];
+
+const customers = []
+
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if (operation.type === 'credit') {
+            return acc + operation.amount
+        }
+        else {
+            return acc - operation.amount
+        }
+    }, 0)
+
+    return balance
+}
 
 function verifyIfExistsAccountCPF(request, response, next) {
     const { cpf } = request.headers;
@@ -31,6 +46,7 @@ app.post("/account", (request, response) => {
         return response.status(400).json({ error: 'Customer Already Exists!' })
     }
 
+
     customers.push({
         name,
         cpf,
@@ -43,12 +59,56 @@ app.post("/account", (request, response) => {
     return response.status(201).send()
 })
 
+//Criar Deposito na Conta
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+    const { customer } = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit",
+    };
+
+    customer.statement.push(statementOperation)
+
+    return response.status(201).send();
+})
+
+//Criar Saque na Conta
+app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
+    const { amount } = request.body
+    const { customer } = request
+
+    const balance = getBalance(customer.statement)
+
+    if (balance < amount) {
+        return response.status(400).json({ error: "Insufficient funds!" })
+    }
+
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit",
+    }
+
+    customer.statement.push(statementOperation)
+
+    return response.status(201).send()
+})
+
+
 //Listar Extrato por CPF
-app.get('/statement/', verifyIfExistsAccountCPF, (request, response) => {
+app.get("/statement/", verifyIfExistsAccountCPF, (request, response) => {
     const { customer } = request
 
     return response.json(customer.statement)
 })
+
+
+
 
 
 app.listen(3333)
