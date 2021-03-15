382570c7c86671982fe4b88656b83e91e2d6345c
guidsantos <guilherme.fast2503@gmail.com>
2021-03-15 19:36:42 -0300

    Create Account / List Statement

--- src/index.js
+++ src/index.js
@@ -1,13 +1,54 @@
-import express from express
+const express = require('express')
+const { v4: uuidv4 } = require('uuid')
 
 const app = express()
 
-app.use(express.json)
+app.use(express.json())
 
-app.post('/account', () => {
+const customers = [];
 
+function verifyIfExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf)
+
+    if (!customer) {
+        return response.status(400).json({ error: "Invalid Customer Information" })
+    }
+
+    request.customer = customer;
+
+    return next()
+}
+
+//Criar conta do cliente
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
+
+    const customerAlreadyExists = customers.some(customer => customer.cpf === cpf)
+
+    if (customerAlreadyExists) {
+        return response.status(400).json({ error: 'Customer Already Exists!' })
+    }
+
+    customers.push({
+        name,
+        cpf,
+        id: uuidv4(),
+        statement: [],
+    })
+
+
+
+    return response.status(201).send()
 })
 
-app.listen(3333, () => {
-    console.log('Servidor Iniciado na porta 3333')
+//Listar Extrato por CPF
+app.get('/statement/', verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request
+
+    return response.json(customer.statement)
 })
+
+
+app.listen(3333)
